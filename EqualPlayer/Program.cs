132617153821b00b238221804a5e615eadab55dd
Syndher <miguelfilipe.dev@gmail.com>
2025-05-09 00:53:36 +0100

    HashSet setOfPlayers

--- EqualPlayer/Program.cs
+++ EqualPlayer/Program.cs
@@ -6,7 +6,14 @@ namespace EqualPlayer
     {
         private static void Main(string[] args)
         {
-            
+            HashSet<Player> setOfPlayers = new HashSet<Player>();
+            setOfPlayers.Add(new Player(PlayerClass.Tank, "Ana"));
+            setOfPlayers.Add(new Player(PlayerClass.Slayer, "Paulo"));
+            setOfPlayers.Add(new Player(PlayerClass.Tank, "Ana"));
+            foreach (Player p in setOfPlayers)
+            {
+                Console.WriteLine($"{p.Name} is a {p.PClass}");
+            }
         }
     }
 }
