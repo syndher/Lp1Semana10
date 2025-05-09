0cd867184955821fe9b1aba2b37eaaeca231944a
Syndher <miguelfilipe.dev@gmail.com>
2025-05-09 01:10:27 +0100

    FilePower2 working

---
+++ FilePower2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FilePower1
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            string filePath = args[0];
+            using (StreamWriter sw = new StreamWriter(filePath))
+            {
+                while(true)
+                {
+                    string input = Console.ReadLine();
+                    if (string.IsNullOrEmpty(input)) break;
+                    sw.WriteLine(input);
+                }
+            }
+        }
+    }
+}
+
