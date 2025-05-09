396d2836bb21ef1ee9d071a4020cb30ec574b06a
Syndher <miguelfilipe.dev@gmail.com>
2025-05-09 01:06:13 +0100

    FilePower1 working

---
+++ FilePower1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FilePower1
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            string filePath = args[0];
+            Queue<string> strings = new Queue<string>();
+            while(true)
+            {
+                string input = Console.ReadLine();
+                if (string.IsNullOrEmpty(input)) break;
+                strings.Enqueue(input);
+            }
+            File.WriteAllLines(filePath, strings);
+        }
+    }
+}
+
